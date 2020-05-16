2dfb87d5234cb96a6bbc28062729d734f94e3132
MarkusRedluxan <markusredluxan@hotmail.com>
2020-05-16 10:58:52 -0400

    Write basic movements in Player.gd
    Read input directions and move player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,19 @@
 extends KinematicBody2D
 
+const SPEED = 70
+
+var velocity := Vector2()
+
 func _ready():
 	print("Player: ready")
 
 func _physics_process(delta):
-	pass
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	var up = Input.is_action_pressed("ui_up")
+	var down = Input.is_action_pressed("ui_down")
+	
+	velocity.x = int(right) - int(left)
+	velocity.y = int(down) - int(up)
+	
+	move_and_collide(velocity * SPEED * delta)
