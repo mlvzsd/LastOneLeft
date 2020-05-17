bf0dd48b63efb92dcfb0db92001929f991cdef03
MarkusRedluxan <markusredluxan@hotmail.com>
2020-05-17 10:32:34 -0400

    Write basic follow in Guard.gd
    Move guard to the target's position

--- scripts/Guard.gd
+++ scripts/Guard.gd
@@ -1,10 +1,17 @@
 extends KinematicBody2D
 
+const SPEED = 50
+
+var velocity := Vector2()
+var target : KinematicBody2D
+
 func _ready():
 	print("Guard[", get_instance_id(), "]: ready")
 
 func _physics_process(delta):
-	pass
+	velocity = (target.position - position).normalized()
+	
+	move_and_collide(velocity * SPEED * delta)
 
 func _on_BodyArea_area_entered(area):
 	print("Guard[", get_instance_id(), "]: hitted")
